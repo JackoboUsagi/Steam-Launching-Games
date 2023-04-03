0fdb246b192a49af3219c279593493dac8f80010
Jackobo Usagi <117809035+Usagi-Corporation@users.noreply.github.com>
2023-04-03 11:55:46 +0200

    Update steamopengame.bat

--- steamopengame.bat
+++ steamopengame.bat
@@ -1,3 +1,6 @@
+REM Désactiver l'affichage des sorties de commande
 @echo off
-Start /Min "" "L:\steambpmclose.bat"
+REM Démarrage du ficher bat pour la fermeture automatique de la fenêtre Steam : Mode Big Picture
+Start /Min "" "steambpmclose.bat"
+REM Exécution du jeu / de l'application Steam avec tous les arguments ( sans aucune modification )
 Start "" %*
