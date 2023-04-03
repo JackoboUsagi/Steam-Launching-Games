0c2d56e6a70907dcd699c23d7ac889d530ef553e
Jackobo Usagi <117809035+Usagi-Corporation@users.noreply.github.com>
2023-04-03 11:53:58 +0200

    Update steambpmclose.bat

--- steambpmclose.bat
+++ steambpmclose.bat
@@ -1,4 +1,8 @@
+REM Désactiver l'affichage des sorties de commande
 @echo off
+REM 10 secondes avant l'exécution de la commande pour laisser le temps à l'application de s'exécuter
 timeout /t 10 /nobreak >nul
+REM Fermeture des fenêtres contenant le mot Steam
 taskkill /fi "WINDOWTITLE eq Steam*"
+REM Fermeture de la console
 exit
